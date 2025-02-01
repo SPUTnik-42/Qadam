1e983244209879dbda0ca8817fc6daeead91c09d
SPUTnik-42 <ushniknath@gmail.com>
2025-02-01 21:59:37 +0530

    second commit

--- qadam_site/Static/Home.js
+++ qadam_site/Static/Home.js
@@ -12,35 +12,3 @@ const navLinks = document.querySelector('.nav-links');
 menuToggle.addEventListener('click', () => {
     navLinks.classList.toggle('active');
 });
-
-
-// Slideshow functionality
-const slides = document.querySelectorAll('.slide');
-const prevButton = document.querySelector('.prev-slide');
-const nextButton = document.querySelector('.next-slide');
-let currentSlide = 0;
-
-function showSlide(index) {
-    slides.forEach(slide => slide.classList.remove('active'));
-    
-    if (index >= slides.length) currentSlide = 0;
-    if (index < 0) currentSlide = slides.length - 1;
-    
-    slides[currentSlide].classList.add('active');
-}
-
-prevButton.addEventListener('click', () => {
-    currentSlide--;
-    showSlide(currentSlide);
-});
-
-nextButton.addEventListener('click', () => {
-    currentSlide++;
-    showSlide(currentSlide);
-});
-
-// Auto advance slides
-setInterval(() => {
-    currentSlide++;
-    showSlide(currentSlide);
-}, 5000); // Change slide every 5 seconds
