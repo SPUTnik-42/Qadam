a25b1b270f899a374feb5994fb223637301a5a31
SPUTnik-42 <ushniknath@gmail.com>
2025-02-05 23:17:14 +0530

    render_updates_2.1

--- sarthak/HomePage.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('logo-link').addEventListener('click', function (event) {
-    event.preventDefault(); // Prevent default link behavior
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth', // Smooth scrolling
-    });
-});
