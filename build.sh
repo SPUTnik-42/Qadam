58bda4319f9b8b91d88ae88ce221edb9f493f026
SPUTnik-42 <ushniknath@gmail.com>
2025-02-16 21:52:55 +0530

    build_fu

--- build.sh
+++ build.sh
@@ -3,5 +3,3 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
-
-python manage.py createsuperuser --noinput --username "admin" --password "1234" --email "admin1234@gmail.com"
